9c7b06caaf0885bdbd56a1206595c2a6727ce898
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-02 09:49:10 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let total = 0;
+const price = document.querySelector('[data-ns-test="prices"]');
+price.forEach(price =>{
+    total += Number(price.textContent);
+});
+const table = document.querySelector('table');
+const row = table.insertRow();
+const cell = row.insertCell();
+cell.colSpan = 2;
+cell.textContent = `Total: ${total}`;
+cell.setAttribute('data-ns-test', 'grandTotal');
